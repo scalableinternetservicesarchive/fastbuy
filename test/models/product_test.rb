b1158ca8a9dba2973f27bb933f2d5cb05f920cd9
cs290 <cs290@localhost.localdomain>
2015-10-10 20:50:04 -0400

    added validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,96 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:quantity].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg",
+                          rating:      1.0,
+                          quantity:     1)
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], 
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "product quantity must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg",
+                          rating:      1.0,
+                          price:     1)
+    product.quantity = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 1"],
+      product.errors[:quantity]
+
+    product.quantity = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 1"], 
+      product.errors[:quantity]
+
+    product.quantity = 1
+    assert product.valid?
+  end
+
+  test "product rating must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg",
+                          quantity:    1,
+                          price:     1)
+    product.rating = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.0"],
+      product.errors[:rating]
+
+    product.rating = 5.1
+    assert product.invalid?
+    assert_equal ["must be less than or equal to 5.0"], 
+      product.errors[:rating]
+
+    product.rating = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1,
+                image_url:   image_url,
+                quantity:    1)
+  end
+
+  #test "image url" do
+  #  ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+  #           http://a.b.c/x/y/z/fred.gif }
+  #  bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  #  
+  #  ok.each do |name|
+  #    assert new_product(name).valid?, "#{name} should be valid"
+  #  end
+
+  #  bad.each do |name|
+  #    assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  #  end
+  #end
+  
 end
