2e648d17e413e71f67a4b761bb9b612f2af36e75
Andre Calfa <aacalfa@gmail.com>
2015-10-16 01:20:26 -0400

    Added line item model tests

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
+  validates :product_id, :cart_id, presence: true, numericality: {greater_than_or_equal_to: 1}
+  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 1}
 
   def total_price
     product.price * quantity
