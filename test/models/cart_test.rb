4ac4747264e372597399067d19b79327c1cde981
Andre Calfa <aacalfa@gmail.com>
2015-10-17 00:47:29 -0400

    Added cart model tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cart add products test" do
+    product = Product.new(id: 1)
+    cart = Cart.new(id: 1)
+    line_item = LineItem.new(product: product, cart: cart)
+    line_item.product_id = product.id
+    assert line_item.valid?
+    cart.add_product(product.id)
+    assert cart.valid?
+  end
+
+  test "cart delete items test" do
+    line_item = LineItem.new
+    line_item.product = products(:one)
+    line_item.build_cart
+
+    line_item.cart.line_items.destroy_all()
+    assert line_item.cart.valid?
+    assert line_item.cart.line_items.empty?
+  end
 end
