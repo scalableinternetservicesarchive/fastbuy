ac0472aeb458bb238f15eba6983a5966eee46a08
Longfeng Li <longfeng_li@umail.ucsb.edu>
2015-10-17 22:19:21 -0400

    added payment

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,10 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
-  validates :product_id, :cart_id, presence: true, numericality: {greater_than_or_equal_to: 1}
-  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 1}
 
+  validates :product_id, presence: true, numericality: {greater_than_or_equal_to: 1}
+  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 1}
   def total_price
     product.price * quantity
   end
