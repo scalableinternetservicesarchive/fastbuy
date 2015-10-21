0985c9d7f0ad37f8fc7d332401268c60e1b554e9
Geurney <dongqiao.ma@gmail.com>
2015-10-21 00:53:01 -0700

    Modified CurrentCart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,10 +4,25 @@ module CurrentCart
     private
 
     def set_cart
-        @cart = Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-        @cart = Cart.create
-        session[:cart_id] = @cart.id
+      puts "@@-Set Cart-@@"
+      if current_buyer == nil
+        @cart = session[:cart]  
+        if @cart == nil
+    	   @cart = Hash.new
+    	   session[:cart] = @cart
+           puts '@@-New Cart-@@'
+        else
+           puts '@@-Get Cart-@@'
+    	end
+      else
+        @cart = Cart.find(current_buyer.cart_id)
+		puts '@@-Current User ' + current_buyer.id.to_s + ' Cart ID ' + current_buyer.cart_id.to_s + '-@@'
+      end
+      rescue ActiveRecord::RecordNotFound
+        @cart = Cart.create(user_id: current_buyer.id)
+        current_buyer.cart_id = @cart.id
+	    current_buyer.save
+	    puts '@@-Create User '+ current_buyer.id.to_s +  ' Cart ID ' + current_buyer.cart_id.to_s + '-@@'
     end
 end
 
