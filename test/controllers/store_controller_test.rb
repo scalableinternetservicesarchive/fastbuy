c087a032db25707a6dc211fbe76bb991963359bd
Gongxl <xuelun.gong@gmail.com>
2015-10-22 03:03:32 -0400

    Use AJAX to +/- line items and add products of desired number to the cart

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -15,7 +15,6 @@ class StoreControllerTest < ActionController::TestCase
   test "markup needed for store.js.coffee is in place" do
     get :index
     assert_select '.store .entry > img', 3
-    assert_select '.entry input[type=submit]', 3
   end
 
 end
