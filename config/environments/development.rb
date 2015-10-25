c73c7524ead86ef9f297090bbb559fe427d0da56
ShenghanGao <victorgaohk@gmail.com>
2015-10-25 15:41:11 +0800

    Use Gmail to replace NetEase Mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,19 +40,20 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-
+  config.action_view.raise_on_missing_translations = true
+  
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smap.163.com",
-    port: 25,
+    address: "smtp.gmail.com",
+    port: 587,
     domain: "127.0.0.1",
     authentication: "plain",
-    user_name: "fastbuy_support@163.com",
-    password: "FastBuy",
+    user_name: "fastbuy.noreply@gmail.com",
+    password: "FastBuyNoReply",
     enable_starttls_auto: true
-} # config.action_view.raise_on_missing_translations = true
-  
+}
+
   config.action_mailer.perform_deliveries = true
 end
 
