167e013c374d4def2554ee9243aa4d6c5724e3bb
ShenghanGao <victorgaohk@gmail.com>
2015-10-25 17:36:31 +0800

    Fix test cases, BUT IMPROVMENTS NEEDED

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,22 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Your FastBuy Order Confirmation", mail.subject
+    assert_equal ["john@site.tw"], mail.to
+    puts mail.to.class
+    assert_equal ["fastbuy.noreply@gmail.com"], mail.from
+    # TODO: NEED EMAIL CONTENTS CHECKING
+    assert_match /Dear John,\s/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Your FastBuy Order Shipped", mail.subject
+    assert_equal ["john@site.tw"], mail.to
+    assert_equal ["fastbuy.noreply@gmail.com"], mail.from
+    # TODO: NEED EMAIL CONTENTS CHECKING
+    assert_match /<h3>FastBuy Order Shipped<\/h3>\s/, mail.body.encoded
   end
 
 end
