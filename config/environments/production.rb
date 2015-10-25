59fedcefddb05ae818d4e545dd1f6b0e722a9390
ShenghanGao <victorgaohk@gmail.com>
2015-10-25 17:00:24 +0800

    Add email info to production mode configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "127.0.0.1",
+    authentication: "plain",
+    user_name: "fastbuy.noreply@gmail.com",
+    password: "FastBuyNoReply",
+    enable_starttls_auto: true
+}
+
+  config.action_mailer.perform_deliveries = true
+
 end
+
