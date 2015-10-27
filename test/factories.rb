23d6f87edeccf04cdb32d1de00c9676dd8b7f0cf
Gongxl <xuelun.gong@gmail.com>
2015-10-27 06:46:34 -0400

    Generate the scaffold for sale products

--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,10 @@
-FactoryGirl.define do
+FactoryGirl.define do  factory :sale_product do
+    product_id 1
+price "9.99"
+quantity 1
+expire_time "2015-10-26 11:17:58"
+  end
+
   factory :buyer do
   email 'test@example.com'
   password 'f4k3p455w0rd'
