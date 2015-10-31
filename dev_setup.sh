8850cfdddfaf73509ab12f8fe54c9e233734e43c
Geurney <Geurney@users.noreply.github.com>
2015-10-30 17:08:46 -0700

    Update dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,6 +1,8 @@
 #To run:
-#sh dev_setup.sh
+#. dev_setup.sh
 #Notice: In VM, the env is development as default.
+#Install mysql if bundle fails.
+#sudo yum install mysql-devel
 
 RAILS_ENV=development rake sunspot:solr:stop
 RAILS_ENV=development rake sunspot:solr:start
