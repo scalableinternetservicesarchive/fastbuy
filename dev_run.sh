0e905053bf0d3c5b84e8f2741301c70e3edef743
Geurney <dongqiao.ma@gmail.com>
2015-10-31 14:33:59 -0400

    Seperatly start solr service

--- dev_run.sh
+++ dev_run.sh
@@ -1,5 +1,6 @@
-#To run:
-#sh dev_run
-#Notice: In VM, the env is development as default.
+# Run Development Mode
+# To run:
+# sh dev_run
+# Notice: In VM, the env is development as default.
 
-RAILS_ENV=development bundle exec rails s -b 0.0.0.0
+rails s -b 0.0.0.0 -e development
