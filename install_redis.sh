3af9440a6d21df518c36dc19477f9d4260ca0e91
ShenghanGao <victorgaohk@gmail.com>
2015-11-02 04:47:18 +0800

    Try to run on AWS

--- install_redis.sh
+++ install_redis.sh
@@ -2,14 +2,13 @@
 
 FASTBUY_DIR=$PWD
 cd ..
-sudo yum install tcl
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 rm redis-stable.tar.gz
 cd redis-stable
 make
 make test
-REDIS_PATH=$PWD/src
+export REDIS_PATH=$PWD/src
 echo $REDIS_PATH
 cd $FASTBUY_DIR
 
