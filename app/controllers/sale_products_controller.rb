3ffc50c7531b85faed0e9b4553189eb23355649c
Geurney <dongqiao.ma@gmail.com>
2015-11-01 16:25:46 -0500

    Sale product display

--- app/controllers/sale_products_controller.rb
+++ app/controllers/sale_products_controller.rb
@@ -1,5 +1,7 @@
 class SaleProductsController < ApplicationController
+  before_action :authenticate_seller!, except: [:index]
   before_action :set_sale_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:new]
 
   # GET /sale_products
   # GET /sale_products.json
@@ -14,7 +16,7 @@ class SaleProductsController < ApplicationController
 
   # GET /sale_products/new
   def new
-    @sale_product = SaleProduct.new
+    @sale_product = @product.nil? ? SaleProduct.new() : SaleProduct.newSale(@product)
   end
 
   # GET /sale_products/1/edit
@@ -25,7 +27,6 @@ class SaleProductsController < ApplicationController
   # POST /sale_products.json
   def create
     @sale_product = SaleProduct.new(sale_product_params)
-
     respond_to do |format|
       if @sale_product.save
         format.html { redirect_to @sale_product, notice: 'Sale product was successfully created.' }
@@ -65,6 +66,10 @@ class SaleProductsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_sale_product
       @sale_product = SaleProduct.find(params[:id])
+    end
+
+    def set_product
+      @product = Product.find(params[:product_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
