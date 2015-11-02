d5d790103b2ec918c6823bdf4dc047e49fa1eff3
Andre Calfa <aacalfa@gmail.com>
2015-11-02 01:00:21 -0500

    Adding buyer and seller seeds, product description also searchable

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,7 @@ class Product < ActiveRecord::Base
   # Solr Search
   searchable do
     text :title, :as => :title_textp
+    text :description, :as => :description_textp
     boolean :on_sale
   end
 
