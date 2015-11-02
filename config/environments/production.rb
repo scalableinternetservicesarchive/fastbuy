0ca4ee903431b5f0cc49768df639e9775daef231
ShenghanGao <victorgaohk@gmail.com>
2015-11-02 17:05:40 +0800

    Adjust for AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -92,6 +92,8 @@ Rails.application.configure do
 }
 
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default :charset => "utf-8"
 
 end
 
--- start_sidekiq.sh
+++ start_sidekiq.sh
@@ -5,6 +5,7 @@ export REDIS_PATH=$HOME/redis-stable/src
 echo "Starting Redis..."
 $REDIS_PATH/redis-server > /dev/null 2>&1 &
 #sleep 3
+mkdir -p tmp/pids
 echo "Starting Sidekiq..."
 bundle exec sidekiq > /dev/null 2>&1 &
 #sleep 3
