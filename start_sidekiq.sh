5fd2939e8395c3d61b4da95935f4c23e6a0a9fb9
ShenghanGao <victorgaohk@gmail.com>
2015-11-02 16:14:29 +0800

    Add sidekiq settings

--- start_sidekiq.sh
+++ start_sidekiq.sh
@@ -6,6 +6,6 @@ echo "Starting Redis..."
 $REDIS_PATH/redis-server > /dev/null 2>&1 &
 #sleep 3
 echo "Starting Sidekiq..."
-bundle exec sidekiq -q default -q mailers > /dev/null 2>&1 &
+bundle exec sidekiq > /dev/null 2>&1 &
 #sleep 3
 
