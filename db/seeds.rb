c629c4e65e8472054067b9f2dd50e69242a8f4e8
Geurney <dongqiao.ma@gmail.com>
2015-11-04 15:16:42 -0500

    New Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,105 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.delete_all
-string = "CoffeeScript is JavaScript done right. It provides all of JavaScript's functionality wrapped in a cleaner, 
-		more succinct syntax. In the first book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript while writing clearer, cleaner, 
-	and safer code. Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox. 
-		Our interactive magazine experience allows you to start reading in just a few seconds with access
-		to any back issue at any time on the cloud. Family Library links your Amazon account to that 
-		of your spouse or partner so you can easily share apps, games, audiobooks, and books, and it 
-		now allows Prime members to share their Prime Video content. The moment you use iPhone 6s, you know you’ve never felt anything like it. With a single press, 3D Touch lets you do more than ever before. Live Photos bring your memories to life in a powerfully vivid way. And that’s just the beginning. Take a deeper look at iPhone 6s, and you’ll find innovation on every level. With 3D Touch, you can do things that were never possible before. It senses how deeply you press the display, letting you do all kinds of essential things more quickly and simply. And it gives you real-time feedback in the form of subtle taps from the all-new Taptic Engine.  You’ll experience up to 70 percent faster CPU performance, and up to 90 percent faster GPU performance for all your favorite graphics-intensive games and apps. ".split(/\W+/)
-words = 50
-if Rails.env == 'production'
-  num = 100
-else
-  num = 20
-end
-for i in 1..num
-  describe=""
-  words.times{ describe << string[rand(0...string.length)] +" "}
-  Product.create(title: "Item#{i}",
-    description: 
-    %{<p>
-        #{describe}
-      </p>},
-       image_url: "/assets/images/#{i}.jpg",    
-     price: rand(0..9)*100 + rand(0..9)*10 + rand(1..9) + rand(1..99)/100.0,
-     quantity: rand(1..100),
-     rating: rand(1..4) + rand(1..9)/10.0)
-end
+Buyer.delete_all
+Seller.delete_all
 
 # Seed buyers and sellers
 psswd = "12345678"
 Buyer.create(email: "1@g.com", password: "#{psswd}")
 Seller.create(email: "2@g.com", password: "#{psswd}")
 
-usernames = (1..1000).to_a
+usernames = (1..100).to_a
 provider = ('a'..'a').to_a
-for j in 0...provider.length
-  for i in 0...usernames.length
-    Buyer.create(email: "#{usernames[i]}"+"@#{provider[j]}.com", password: "#{psswd}")
-    Seller.create(email: "#{usernames[i]}"+"@#{provider[j]}.com", password: "#{psswd}")
+enpsswd = Buyer.new.send(:password_digest, '12345678')
+
+# Original
+start_time = Time.now
+1000.times do |i| 
+  Buyer.create(email: "#{usernames[i]}"+"@#{provider[i]}.com", password: "#{psswd}")
+end
+end_time = Time.now
+elapse = (end_time - start_time)
+puts "Original: #{elapse}"
+=begin
+Buyer.delete_all
+# Use Transaction
+start_time = Time.now
+ActiveRecord::Base.transaction do
+  Buyer.create(email: "#{usernames[i]}"+"@#{provider[i]}.com", password: "#{psswd}")
+end
+end_time = Time.now
+elapse = (end_time - start_time)
+puts "Transcation: #{elapse}"
+
+Buyer.delete_all
+# Use No Validation
+start_time = Time.now
+1000.times do |i| 
+  buyer = Buyer.new(email: "#{usernames[i]}"+"@#{provider[i]}.com", password: "#{psswd}")
+  buyer.save(validate: false)
+end
+end_time = Time.now
+elapse = (end_time - start_time)
+puts "No-validation: #{elapse}"
+
+Buyer.delete_all
+# Use No Validation and Transaction
+start_time = Time.now
+ActiveRecord::Base.transaction do
+  1000.times do |i| 
+    buyer = Buyer.new(email: "#{usernames[i]}"+"@#{provider[i]}.com", password: "#{psswd}")
+    buyer.save(validate: false)
   end
 end
+end_time = Time.now
+elapse = (end_time - start_time)
+puts "No-validation and Transaction: #{elapse}"
+
+Buyer.delete_all
+# Use Encrypted Password
+start_time = time.now
+1000.times do |i| 
+  Buyer.create(email: "#{usernames[i]}"+"@#{provider[i]}.com", encrypted_password: "#{enpsswd}")
+end
+end_time = time.now
+elapse = (end_time - start_time)
+puts "Encrypted: #{elapse}"
+
+
+Buyer.delete_all
+# Use Encrypted Password and Transaction
+start_time = time.now
+ActiveRecord::Base.transaction do
+  1000.times do |i| 
+    Buyer.create(email: "#{usernames[i]}"+"@#{provider[i]}.com", encrypted_password: "#{enpsswd}")
+  end
+end
+end_time = time.now
+elapse = (end_time - start_time)
+puts "Encrypted and Transaction: #{elapse}"
+
+Buyer.delete_all
+# Use Encrypted Password and No-Validation
+start_time = time.now
+  1000.times do |i| 
+    buyer = Buyer.new(email: "#{usernames[i]}"+"@#{provider[i]}.com", encrypted_password: "#{enpsswd}")
+    buyer.save(validation: false)
+  end
+end_time = time.now
+elapse = (end_time - start_time)
+puts "Encrypted and No-validation: #{elapse}"
+
+Buyer.delete_all
+# Use Encrypted Password and No-Validation and Transaction
+start_time = time.now
+ActiveRecord::Base.transaction do
+  1000.times do |i| 
+    buyer = Buyer.new(email: "#{usernames[i]}"+"@#{provider[i]}.com", encrypted_password: "#{enpsswd}")
+    buyer.save(validation: false)
+  end
+end
+end_time = time.now
+elapse = (end_time - start_time)
+puts "Encrypted and No-validation and Transaction: #{elapse}"
+=end
