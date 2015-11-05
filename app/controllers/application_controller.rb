aa80b5883acc96e06c953b70a2e7f8e1e930a4e9
Andre Calfa <aacalfa@gmail.com>
2015-11-04 19:17:28 -0500

    attempt to fix tsung 422 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # Note: Temporarily commenting this to see if the 422 errors in tsung are fixed.
   #protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 end
