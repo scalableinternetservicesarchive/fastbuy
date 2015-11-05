4f3cfb6ffffe4be250a2665d634e5b31ac524fd3
Longfeng Li <longfeng_li@umail.ucsb.edu>
2015-11-05 03:07:06 -0500

    improved pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.paginate(:page => params[:page], :per_page => 20)
+    @products = Product.paginate(:page => params[:page], :per_page => 10)
 
       respond_to do |format|
 	format.html #index.html.erb
