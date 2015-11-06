1a70c44f6ef6eea0c4cee979f349d1a807f2c1f0
Geurney <dongqiao.ma@gmail.com>
2015-11-06 18:28:07 -0500

    Fixed dev_test.sh

--- dev_test.sh
+++ dev_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # To run
 # . dev_test.sh
-if ps -ef | grep solr | grep test | awk '{$2}'; then
+if ps -ef | grep solr | grep -q test; then
   echo Solr Test is running!
 else
   rake sunspot:solr:start RAILS_ENV=test 
