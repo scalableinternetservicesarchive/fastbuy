0877e0cb6ab632c481bfba5c08c8922eb54a2598
Longfeng Li <longfeng_li@umail.ucsb.edu>
2015-11-08 00:34:53 -0500

    add order unit test

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,12 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check" , "Credit card" , "Purchase order" ]
   has_many :line_items, dependent: :destroy
-  validates :name, :address, :email, presence: true
+  validates :name, :address, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   validates :pay_type, inclusion: PAYMENT_TYPES
+
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,8 +1,60 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @order = orders(:one)
+  end
+
+  test "order attributes must not be empty" do
+    order = Order.new
+    assert order.invalid?
+    assert order.errors[:name].any?
+    assert order.errors[:address].any?
+    assert order.errors[:email].any?
+    assert order.errors[:pay_type].any?
+  end
+
+  test "should be valid" do
+    assert @order.valid?
+  end
+
+  test "name should be present" do
+    @order.name = "  "
+    assert_not @order.valid?
+  end
+ 
+  test "address should be present" do 
+    @order.address = "  "
+    assert_not @order.valid?
+  end 
+  
+  test "email should be present" do
+    @order.email = " "
+    assert_not @order.valid?
+  end
+
+  test "email should not be too long" do
+    @order.email = "a" * 244 + "@example.com"
+    assert_not @order.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_address = %w[order@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_address.each do |valid_address|
+      @order.email = valid_address
+      assert @order.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "pay_type should be valid" do
+    @order.pay_type = "bank"
+    assert_not @order.valid?
+  end
+  
+  test "pay_type should be right" do
+    @order.pay_type = "Check"
+    assert @order.valid?
+  end
+	
 end
 
