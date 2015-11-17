d6d86787bdf135fc3ea90f4cd9c1ed2cc8946c36
Gongxl <xuelun.gong@gmail.com>
2015-11-17 02:53:17 -0500

    Add server-side caching for store index

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,13 @@
 module StoreHelper
+
+  def cache_key_for_products
+    count = Product.count
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_product(prod)
+    "product/#{prod.id}/#{prod.updated_at}/#{prod.sale_products.count}"
+  end
+
 end
