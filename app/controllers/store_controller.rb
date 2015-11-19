9966641a7081216512ffa31092f3ba10e641430e
Geurney <dongqiao.ma@gmail.com>
2015-11-19 01:00:36 -0500

    Added product server cache

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,17 +6,17 @@ class StoreController < ApplicationController
   def index
     search_param = params[:search] ? params[:search].squish : nil
     if search_param == nil
-      @products = Product.paginate(page:params[:page], per_page:100)
+      @products = Product.paginate(page:params[:page], per_page:20)
     else
       if search_param == 'on_sale'
-        @search = Product.search do
+        @search = Product.search(include: [:sale_products]) do
           with(:on_sale, true)
-          paginate :page => params[:page], :per_page => 100
+          paginate :page => params[:page], :per_page => 20
         end
       else
         @search = Product.search do
           fulltext search_param
-          paginate :page => params[:page], :per_page => 100
+          paginate :page => params[:page], :per_page => 20
         end
       end
       @products = @search.results
@@ -25,8 +25,7 @@ class StoreController < ApplicationController
   
   def sort
     sort_type = SORT_TYPE[params[:sort]]
-    @products = Product.order(sort_type).paginate(page:params[:page], per_page:100)
+    @products = Product.order(sort_type).paginate(page:params[:page], per_page:20)
     render 'index'
   end
 end
-
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+
+  def cache_key_for_products
+    count = Product.where(seller: current_seller).count;
+    max_updated_at = Product.where(seller: current_seller).maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_product(prod)
+    "product/#{prod.id}/#{prod.updated_at}/#{prod.sale_products.count}"
+  end
+
 end
