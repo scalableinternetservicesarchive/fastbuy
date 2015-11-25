1a1d4a8a308da1d09542ed95e2732bc8b0aa35ed
Geurney <dongqiao.ma@gmail.com>
2015-11-25 02:05:18 -0500

    Fixed sort

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,32 +5,34 @@ class StoreController < ApplicationController
   
   def index
     expires_in 3.minutes, public: true, must_revalidate: true
-    search_param = params[:search] ? params[:search].squish : nil
-    if search_param == nil
-      @products = Product.includes(:sale_products).paginate(page:params[:page], per_page:20) if stale?([Product.includes(:sale_products).paginate(page:params[:page], per_page:20), @cart, @cart.class == Cart ? @cart.line_items : nil, current_seller, current_buyer])
+    @page = params[:page] ? params[:page] : 1
+    @search_param = params[:search] ? params[:search].squish : nil
+    if @search_param == nil
+      @products = Product.includes(:sale_products).paginate(page: @page, per_page:20) if stale?([Product.includes(:sale_products).paginate(page: @page, per_page:20), @cart, @cart.class == Cart ? @cart.line_items : nil, current_seller, current_buyer])
      else
-      if search_param == 'on_sale'
+      if @search_param == 'on_sale'
         @search = Product.search(include: [:sale_products]) do
           with(:on_sale, true)
           paginate :page => params[:page], :per_page => 20
         end
       else
         @search = Product.search do
-          fulltext search_param
+          fulltext @search_param
           paginate :page => params[:page], :per_page => 20
         end
       end
       @products = @search.results if stale?([@search, @cart, @cart.class == Cart ? @cart.line_items : nil, current_seller, current_buyer])
     end
-      @page = params[:page]
   end
   
   def sort
-    sort_type = SORT_TYPE[params[:sort]]
-    if stale?([sort_type, Product.order(sort_type).paginate(page:params[:page], per_page:20),@cart, @cart.class == Cart ? @cart.line_items : nil, current_seller, current_buyer]) 
-      @products = Product.order(sort_type).paginate(page:params[:page], per_page:20)
-      render 'index'
-    end
+    @sort_type = SORT_TYPE[params[:sort]]
+    @page = params[:page] ? params[:page] : 1
+   # if stale?([sort_type, Product.order(sort_type).paginate(page:params[:page], per_page:20),@cart, @cart.class == Cart ? @cart.line_items : nil, current_seller, current_buyer]) 
+    @products = Product.order(@sort_type).paginate(page: @page, per_page:20)
+    #  puts "Sort is Stale!"
+    render 'index'
+   # end
   end
 end
 
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,7 +1,7 @@
 module StoreHelper
 
   def cache_key_for_products
-    count = Product.count
+    count = Product.count 
     max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "products/all-#{count}-#{max_updated_at}"
   end
