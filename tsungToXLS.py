3a0a4f47cd7868cbfb96d8a6beb871b183d8753c
Geurney <Geurney@users.noreply.github.com>
2015-12-02 22:56:28 -0800

    Added Highest Request time and fixed bugs

--- tsungToXLS.py
+++ tsungToXLS.py
@@ -10,15 +10,15 @@ from openpyxl import Workbook
 from openpyxl.chart import BarChart, Reference
 
 wb = Workbook()
-source_folder = "F:/tsungtest/"
+source_folder = "F:/tsungtest/newtsung/sqlvsbasic/"
 dest_filename = 'result.xlsx'
-folder_names = ['master-medium','master-large','master-xlarge','cache-medium','cache-large','cache-xlarge']
+folder_names = ['basic-medium','basic-large','basic-xlarge','basic-r3large','basic-c3large','sql-medium','sql-large','sql-xlarge','sql-r3large','sql-c3large']
 for folder_name in folder_names:
     file_name = source_folder + folder_name + "/report.html"
     soup = BeautifulSoup(open(file_name))
     categories = soup.find_all("h3");
     tables = soup.find_all("table", class_="table")
-    if folder_name == 'master-medium':
+    if folder_name == 'basic-medium':
         sheet = wb.active
         sheet.title = folder_name
     else:
@@ -28,6 +28,8 @@ for folder_name in folder_names:
         if index == 1:
             continue
         category = categories[index]
+        if category.text == 'Errors':
+            continue
         if index >= 3:
             table = tables[index + 1]
         else:
@@ -51,44 +53,74 @@ for folder_name in folder_names:
 
 # Anaylysis Sheet
 sheet = wb.create_sheet('analysis', 0)
-rows_name = ['Medium', 'Large', 'xLarge'];
-# Response Time
-sheet.append(['Response', 'Master', 'Cache'])
+rows_name = ['Medium', 'Large', 'xLarge', 'r3Large', 'c3Large'];
+
+# Mean Response Time
+sheet.append(['Mean Response', 'Basic', 'Sql'])
 for index in range(len(folder_names) / 2):
     alist = []
     alist.append(rows_name[index])
     current_sheet = wb.get_sheet_by_name(folder_names[index])
     value = float(current_sheet['F5'].value.partition(' ')[0])
     alist.append(value)
-    current_sheet = wb.get_sheet_by_name(folder_names[index + 3])
+    current_sheet = wb.get_sheet_by_name(folder_names[index + 5])
     value = float(current_sheet['F5'].value.partition(' ')[0])
-    alist.append(value)   
-    sheet.append(alist)
-# 503
-sheet.append(['503', 'Master', 'Cache'])
-for index in range(len(folder_names) / 2):
-    alist = []
-    alist.append(rows_name[index])
-    current_sheet = wb.get_sheet_by_name(folder_names[index])
-    value = float(current_sheet['D29'].value.partition(' ')[0])
-    alist.append(value)
-    current_sheet = wb.get_sheet_by_name(folder_names[index + 3])
-    value = float(current_sheet['D29'].value.partition(' ')[0])
     alist.append(value)   
     sheet.append(alist)
     
-# Draw Response Chart
+# Highest Response Time
+sheet.append(['Highest Response', 'Basic', 'Sql'])
+for index in range(len(folder_names) / 2):
+    alist = []
+    alist.append(rows_name[index])
+    current_sheet = wb.get_sheet_by_name(folder_names[index])
+    value = float(current_sheet['B5'].value.partition(' ')[0])
+    alist.append(value)
+    current_sheet = wb.get_sheet_by_name(folder_names[index + 5])
+    value = float(current_sheet['B5'].value.partition(' ')[0])
+    alist.append(value)   
+    sheet.append(alist)
+
+# 503
+sheet.append(['503', 'Basic', 'Sql'])
+for index in range(len(folder_names) / 2):
+    alist = []
+    alist.append(rows_name[index])
+    current_sheet = wb.get_sheet_by_name(folder_names[index])
+    total_request = float(current_sheet['D27'].value) + float(current_sheet['D28'].value) +  float(current_sheet['D29'].value)
+    value = float(float(current_sheet['D29'].value) / total_request)
+    alist.append(value)
+    current_sheet = wb.get_sheet_by_name(folder_names[index + 5])
+    total_request = float(current_sheet['D27'].value) +  float(current_sheet['D28'].value) +  float(current_sheet['D29'].value)
+    value = float(float(current_sheet['D29'].value) / total_request)
+    alist.append(value)   
+    sheet.append(alist)
+    
+# Draw Mean Response Chart
 response_chart = BarChart()
 response_chart.type = "col"
 response_chart.style = 10
-response_chart.title = "Response Time"
+response_chart.title = "Mean Response Time"
 
-data = Reference(sheet, min_col=2, min_row=1, max_row=4, max_col=3)
-cats = Reference(sheet, min_col=1, min_row=2, max_row=4)
+data = Reference(sheet, min_col=2, min_row=1, max_row=6, max_col=3)
+cats = Reference(sheet, min_col=1, min_row=2, max_row=6)
 response_chart.add_data(data, titles_from_data=True)
 response_chart.set_categories(cats)
 response_chart.shape = 4
 sheet.add_chart(response_chart, "G1")
+
+# Draw Highest Response Chart
+response_chart = BarChart()
+response_chart.type = "col"
+response_chart.style = 10
+response_chart.title = "Highest Response Time"
+
+data = Reference(sheet, min_col=2, min_row=7, max_row=12, max_col=3)
+cats = Reference(sheet, min_col=1, min_row=8, max_row=12)
+response_chart.add_data(data, titles_from_data=True)
+response_chart.set_categories(cats)
+response_chart.shape = 4
+sheet.add_chart(response_chart, "G16")
 
 # Draw 503 Chart
 response_chart = BarChart()
@@ -96,11 +128,11 @@ response_chart.type = "col"
 response_chart.style = 10
 response_chart.title = "503"
 
-data = Reference(sheet, min_col=2, min_row=5, max_row=8, max_col=3)
-cats = Reference(sheet, min_col=1, min_row=6, max_row=8)
+data = Reference(sheet, min_col=2, min_row=13, max_row=18, max_col=3)
+cats = Reference(sheet, min_col=1, min_row=14, max_row=18)
 response_chart.add_data(data, titles_from_data=True)
 response_chart.set_categories(cats)
 response_chart.shape = 4
-sheet.add_chart(response_chart, "G16")
+sheet.add_chart(response_chart, "G32")
 
 wb.save(dest_filename)
